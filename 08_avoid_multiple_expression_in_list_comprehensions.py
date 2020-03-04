6baf8932c2c138a93e1d457cf63dcf47465c22a4
Rudy <rudy.dev.kor@gmail.com>
2020-03-04 17:10:36 +0900

    Update item 08

--- 08_avoid_multiple_expression_in_list_comprehensions.py
+++ 08_avoid_multiple_expression_in_list_comprehensions.py
@@ -1,3 +1,4 @@
+########### simple case of list comprehensive #######
 arr = [[1,2,3], [4,5,6], [7,8,9]]
 flat = [x for row in arr for x in row]
 print(flat)
@@ -5,12 +6,15 @@ print(flat)
 squared = [[x**2 for x in row] for row in arr]
 print(squared)
 
+########### complicated case of list comprehensive #######
+# this multiline comprehsension isn't much shorter than the alternative
 arr2 = [[[1, 2, 3], [4, 5, 6]]]
 complicated_flat = [x for sublist1 in arr2
                         for sublist2 in sublist1
                             for x in sublist2]
 print(complicated_flat)
 
+########### desriable way: just use normal loop statements #######
 desirable_flat = []
 for sublist1 in arr2:
     for sublist2 in sublist1:
