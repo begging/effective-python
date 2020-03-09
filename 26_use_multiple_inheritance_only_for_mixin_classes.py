07287175c88e5c76996380554f1bd6babaca0d44
Rudy <rudy.dev.kor@gmail.com>
2020-03-09 10:55:27 +0900

    Update item 26
     - Add BinaryTreeWithParent
     - Add NamedSubTree
     - Add comments

--- 26_use_multiple_inheritance_only_for_mixin_classes.py
+++ 26_use_multiple_inheritance_only_for_mixin_classes.py
@@ -1,8 +1,12 @@
 from pprint import pprint
 
+
+'''
+An example of Mixin Classes
+'''
 class ToDictMixin:
     def to_dict(self):
-        print(self.__dict__)
+        # print(self.__dict__)
         return self._traverse_dict(self.__dict__)
 
     def _traverse_dict(self, instance_dict):
@@ -12,23 +16,23 @@ class ToDictMixin:
         return output
 
     def _traverse(self, key, value):
-        input()
-        print(value)
+        # input()
+        # print(value)
 
         if isinstance(value, ToDictMixin):
-            print('isinstance(value, ToDictMixin)')
+            # print('isinstance(value, ToDictMixin)')
             return value.to_dict()
         elif isinstance(value, dict):
-            print('isinstance(value, dict)')
+            # print('isinstance(value, dict)')
             return self._traverse_dict(value)
         elif isinstance(value, list):
-            print('isinstance(value, list)')
+            # print('isinstance(value, list)')
             return [self._traverse(key, i) for i in value]
         elif hasattr(value, '__dict__'):
-            print('hasattr(value, __dict__):')
+            # print('hasattr(value, __dict__):')
             return self._traverse_dict(value.__dict__)
         else:
-            print('value')
+            # print('value')
             return value
 
 
@@ -43,3 +47,47 @@ tree = BinaryTree(10,
                   right=BinaryTree(13, left=BinaryTree(11)))
 
 pprint(tree.to_dict())
+print()
+
+
+'''
+The best part about mix-ins is that you can make their generic functionality
+pluggable so behaviors can be overridden when required.
+
+This circular reference would cause the default implementation of
+ToDictMixin.to_dict to loop forever.
+
+The solution is to override the ToDictMixin._traverse method in the
+BinaryTreeWithParent class to only process values that matter, preventing
+cycles encountered by the mix-in.
+'''
+class BinaryTreeWithParent(BinaryTree):
+    def __init__(self, value, left=None, right=None, parent=None):
+        super().__init__(value, left=left, right=right)
+        self.parent = parent
+
+    def _traverse(self, key, value):
+        if (isinstance(value, BinaryTreeWithParent) and key == 'parent'):
+            return value.value # Prevent cycles else:
+        return super()._traverse(key, value)
+
+root = BinaryTreeWithParent(10)
+root.left = BinaryTreeWithParent(7, parent=root)
+root.left.right = BinaryTreeWithParent(9, parent=root.left)
+pprint(root.to_dict())
+print()
+
+
+'''
+By defining BinaryTreeWithParent._traverse, I’ve also enabled any class that
+has an attribute of type BinaryTreeWithParent to automatically work with
+ToDictMixin.
+'''
+class NamedSubTree(ToDictMixin):
+    def __init__(self, name, tree_with_parent):
+        self.name = name
+        self.tree_with_parent = tree_with_parent
+
+my_tree = NamedSubTree('foobar', root.left.right)
+pprint(my_tree.to_dict()) # No infinite loop
+print()
