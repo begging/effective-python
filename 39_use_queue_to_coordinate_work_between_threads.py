bac3cab17af08f7fad3c476a139fba5a7dd7c37b
Rudy <rudy.dev.kor@gmail.com>
2020-03-16 23:50:34 +0900

    Update item 39
     - Add annotation

--- 39_use_queue_to_coordinate_work_between_threads.py
+++ 39_use_queue_to_coordinate_work_between_threads.py
@@ -3,118 +3,126 @@ from threading import Thread, Lock
 from time import sleep
 from queue import Queue
 
-# class MyQueue:
-#     def __init__(self):
-#         self.items = deque()
-#         self.lock = Lock()
+class MyQueue:
+    def __init__(self):
+        self.items = deque()
+        self.lock = Lock()
 
-#     def put(self, item):
-#         with self.lock:
-#             self.items.append(item)
+    def put(self, item):
+        with self.lock:
+            self.items.append(item)
 
-#     def get(self):
-#         with self.lock:
-#             return self.items.popleft()
-
-
-# class Worker(Thread):
-#     def __init__(self, func, in_queue, out_queue):
-#         super().__init__()
-#         self.func = func
-#         self.in_queue = in_queue
-#         self.out_queue = out_queue
-#         self.polled_count = 0
-#         self.work_done = 0
-#         self.error_count = 0
-
-#     def run(self):
-#         while True:
-#             self.polled_count += 1
-#             try:
-#                 item = self.in_queue.get()
-#             except IndexError:
-#                 sleep(0.001) # No work to do
-#                 self.error_count += 1
-#             else:
-#                 result = self.func(item)
-#                 self.out_queue.put(result)
-#                 self.work_done += 1
-
-# def download(item):
-#     return object()
-
-# def resize(item):
-#     return object()
-
-# def upload(item):
-#     return object()
-
-# download_queue = MyQueue()
-# resize_queue = MyQueue()
-# upload_queue = MyQueue()
-# done_queue = MyQueue()
-# threads = [
-#     Worker(download, download_queue, resize_queue),
-#     Worker(resize, resize_queue, upload_queue),
-#     Worker(upload, upload_queue, done_queue)
-# ]
-
-# for thread in threads:
-#     thread.start()
-
-# for _ in range(1000):
-#     download_queue.put(object())
-
-# while len(done_queue.items) < 1000:
-#     # print(len(done_queue.items))
-#     pass
-
-# processed = len(done_queue.items)
-# polled = sum(t.polled_count for t in threads)
-# sum_error_count = sum(t.error_count for t in threads)
-# print('Processed', processed, 'items after polling', polled, 'times')
-# print('error_count', sum_error_count)
+    def get(self):
+        with self.lock:
+            return self.items.popleft()
 
 
-# queue = Queue()
+class Worker(Thread):
+    def __init__(self, func, in_queue, out_queue):
+        super().__init__()
+        self.func = func
+        self.in_queue = in_queue
+        self.out_queue = out_queue
+        self.polled_count = 0
+        self.work_done = 0
+        self.error_count = 0
 
-# def consumer():
-#     print('Consumer waiting')
-#     queue.get() # Runs after put() below
-#     print('Consumer done')
+    def run(self):
+        while True:
+            self.polled_count += 1
+            try:
+                item = self.in_queue.get()
+            except IndexError:
+                sleep(0.001) # No work to do
+                self.error_count += 1
+            else:
+                result = self.func(item)
+                self.out_queue.put(result)
+                self.work_done += 1
 
-# thread = Thread(target=consumer)
-# thread.start()
+def download(item):
+    return object()
 
-# print('Producer putting')
-# queue.put(object())
-# thread.join()
-# print('Producer done')
+def resize(item):
+    return object()
+
+def upload(item):
+    return object()
+
+download_queue = MyQueue()
+resize_queue = MyQueue()
+upload_queue = MyQueue()
+done_queue = MyQueue()
+threads = [
+    Worker(download, download_queue, resize_queue),
+    Worker(resize, resize_queue, upload_queue),
+    Worker(upload, upload_queue, done_queue)
+]
+
+for thread in threads:
+    thread.start()
+
+for _ in range(1000):
+    download_queue.put(object())
+
+while len(done_queue.items) < 1000:
+    # print(len(done_queue.items))
+    pass
+
+processed = len(done_queue.items)
+polled = sum(t.polled_count for t in threads)
+sum_error_count = sum(t.error_count for t in threads)
+print('Processed', processed, 'items after polling', polled, 'times')
+print('error_count', sum_error_count)
 
 
-# '''
-# '''
-# queue = Queue(1) # Buffer size of 1
-# def consumer():
-#     sleep(1) # wait
-#     queue.get() # Runs second
-#     print('Consumer got 1')
-#     queue.get() # Runs fourth
-#     print('Consumer got 2')
+queue = Queue()
 
-# thread = Thread(target=consumer)
-# thread.start()
+def consumer():
+    print('Consumer waiting')
+    queue.get() # Runs after put() below
+    print('Consumer done')
 
-# queue.put(object())
-# print('Producer put 1')
-# queue.put(object())
-# print('Producer put 2')
-# thread.join()
-# print('Producer done')
+thread = Thread(target=consumer)
+thread.start()
+
+print('Producer putting')
+queue.put(object())
+thread.join()
+print('Producer done')
 
 
+'''
+get(): blocking when there's no input
+put(): blocking when queue is full.
+'''
+queue = Queue(1) # Buffer size of 1
+def consumer():
+    sleep(1) # wait
+    queue.get() # Runs second
+    print('Consumer got 1')
+    queue.get() # Runs fourth
+    print('Consumer got 2')
+
+thread = Thread(target=consumer)
+thread.start()
+
+queue.put(object())
+print('Producer put 1')
+queue.put(object())
+print('Producer put 2')
+thread.join()
+print('Producer done')
+
+
+'''
+If you use .join() and don't call .task_done() for every processed item, your
+script will hang forever.
+
+This lets you wait for a phase’s input queue to drain and eliminates the need
+for polling the done_queue at the end of your pipeline.
+'''
 in_queue = Queue()
-
 def consumer():
     print('Consumer waiting')
     work = in_queue.get()
@@ -130,6 +138,8 @@ in_queue.put(object())
 print('Producer waiting')
 in_queue.join()
 print('Producer done')
+
+
 '''
 Closable Queue / Stoppable Thread
 '''
