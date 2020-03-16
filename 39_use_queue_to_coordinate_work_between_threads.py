63c433492d9074e3cc203947cffc541e00cfeafa
Rudy <rudy.dev.kor@gmail.com>
2020-03-16 22:56:00 +0900

    Update item 39

--- 39_use_queue_to_coordinate_work_between_threads.py
+++ 39_use_queue_to_coordinate_work_between_threads.py
@@ -24,6 +24,7 @@ class Worker(Thread):
         self.out_queue = out_queue
         self.polled_count = 0
         self.work_done = 0
+        self.error_count = 0
 
     def run(self):
         while True:
@@ -31,20 +32,21 @@ class Worker(Thread):
             try:
                 item = self.in_queue.get()
             except IndexError:
-                sleep(0.01) # No work to do
+                sleep(0.001) # No work to do
+                self.error_count += 1
             else:
                 result = self.func(item)
                 self.out_queue.put(result)
                 self.work_done += 1
 
 def download(item):
-    return 0
+    return object()
 
 def resize(item):
-    return 0
+    return object()
 
 def upload(item):
-    return 0
+    return object()
 
 download_queue = MyQueue()
 resize_queue = MyQueue()
@@ -63,6 +65,11 @@ for _ in range(1000):
     download_queue.put(object())
 
 while len(done_queue.items) < 1000:
-    print(len(done_queue.items))
+    # print(len(done_queue.items))
+    pass
 
-
+processed = len(done_queue.items)
+polled = sum(t.polled_count for t in threads)
+sum_error_count = sum(t.error_count for t in threads)
+print('Processed', processed, 'items after polling', polled, 'times')
+print('error_count', sum_error_count)
